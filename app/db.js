937038677f656ab50918cb1a89f9dedbf682c31c
Roy Pfaffman <rpfaffman@gmail.com>
2014-01-05 00:17:06 -0800

    Attempting to fix database with Heroku.

--- app/db.js
+++ app/db.js
@@ -2,6 +2,7 @@ var App = require('./config');
 
 var mongoUrl = process.env.MONGOHQ_URL || 'mongodb';
 var mongojs = require('mongojs');
+console.log("<<<<< Attempting to connect database to " + mongoUrl);
 var database = mongojs(mongoUrl, ['projects']);
 
 App.Config.database = database;
